93c4a007ef39dd1acd04a2bdef19945abd9e1ce5
PrBreezy DevOPS Cloud Syst Eng <pr.precival@gmail.com>
2015-12-10 14:20:30 +0100

    Update CreateIISWebsite.ps1

--- CreateIISWebsite.ps1
+++ CreateIISWebsite.ps1
@@ -2,6 +2,7 @@
 param([String]$PublicURL,
       [int]$WebsitePort
     )
+ 
 #Variables
 Import-Module WebAdministration
 $iisAppPoolName = "MyAppPool"
@@ -10,11 +11,13 @@ $WebPath = "c:\inetpub\website"
 $Password = "MyPass"
 $UserName = "MyUser"
 $MyVirtualDirectory = "MyVirtualDirectory"
+
 #Create website with the URL
 New-Item iis:\Sites\$iisAppName -bindings @{protocol="http";bindingInformation=":$WebsitePort:$PublicURL"} -physicalPath $WebPath -id 2
 #Create AppPools
 New-Item IIS:\AppPools\$iisAppPoolName
 Set-ItemProperty IIS:\Sites\$iisAppName -name applicationPool -Value $iisAppPoolName
+
 #Create virtual directory
 New-Item IIS:\Sites\$iisAppName\$MyVirtualDirectory -physicalPath c:\inetpub\img -type VirtualDirectory
 Set-ItemProperty IIS:\Sites\$iisAppName\$MyVirtualDirectory -Name username -Value $UserName
