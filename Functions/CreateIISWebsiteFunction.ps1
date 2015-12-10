0a3f8b9a235d8df056da175efd92c7ca4ad15f78
prbreezy <pr.precival@gmail.com>
2015-12-10 14:38:10 +0100

    create iis website function
    create website function

---
+++ Functions/CreateIISWebsiteFunction.ps1
@@ -0,0 +1,25 @@
+function CreateIISWebsite ($iisAppPoolName, $iisAppName, $WebPath, $Password, $UserName, $MyVirtualDirectory, $PublicURL, $WebsitePort) 
+{ 
+#Variables
+Import-Module WebAdministration
+$iisAppPoolName = "MyAppPool"
+$iisAppName = "MyWebsite"
+$WebPath = "c:\inetpub\website"
+$Password = "MyPass"
+$UserName = "MyUser"
+$MyVirtualDirectory = "MyVirtualDirectory"
+
+
+#Create website with the URL
+New-Item iis:\Sites\$iisAppName -bindings @{protocol="http";bindingInformation=":$WebsitePort:$PublicURL"} -physicalPath $WebPath -id 2
+
+#Create AppPools
+New-Item IIS:\AppPools\$iisAppPoolName
+Set-ItemProperty IIS:\Sites\$iisAppName -name applicationPool -Value $iisAppPoolName
+
+#Create virtual directory
+New-Item IIS:\Sites\$iisAppName\$MyVirtualDirectory -physicalPath c:\inetpub\img -type VirtualDirectory
+Set-ItemProperty IIS:\Sites\$iisAppName\$MyVirtualDirectory -Name username -Value $UserName
+Set-ItemProperty IIS:\Sites\$iisAppName\$MyVirtualDirectory -Name password -Value $Password
+
+}
