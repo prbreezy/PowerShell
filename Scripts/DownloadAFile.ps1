9e3df9a09018c07adee7d7c04959d0e1b733abb9
PrBreezy DevOPS Cloud Syst Eng <pr.precival@gmail.com>
2017-01-16 15:54:56 +0800

    Create DownloadAFile.ps1

---
+++ Scripts/DownloadAFile.ps1
@@ -0,0 +1,5 @@
+#Make sure the target folder exist
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile("https://abcdefghi.jkl./mnopqrstuvw.xyz","C:\temp\abcdefghijklmnopqrstuvw.xyz")
+
